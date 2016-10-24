8dd3aeeb40510f94536bd6403a561c6e8da285ef
ViktoriaK <viktoria.kluckner@gmail.com>
2016-10-24 19:05:38 +0200

    initial commit

---
+++ week_01/basic_types.rb
@@ -0,0 +1,343 @@
+# String
+my_string = "this is a string"
+
+# Fixum/ Integer
+my_fix_num = 100
+
+# Floar
+my_float = 3.1416
+
+# Array (Collections)
+food = ["cheese", "bacon", "wine", "chocolate"]
+lottery_number = [42, 2, 12, 32, 3,1]
+
+# Hash/ Dictionary
+person = {fist_name: "Hans", last_name: "Wurst", age: 20, color: "blue", phone_numbers: [+7924367031, "4178930654"]}
+
+# Assignment 1: Play around with the different variables like in the example below
+# http://ruby-doc.org/core-2.2.0/
+
+puts my_string.scan(/viktoria/)
+
+# Assignment 2: http://learnrubythehardway.org/book/ (exercises 2-17)
+
+# Optional Assignment 3: http://poignant.guide
+
+
+
+# Assignment 2: http://learnrubythehardway.org/book/ (exercises 2-17)
+# 2
+
+puts "I could have code like this."
+
+puts "This will run."
+
+
+# 3
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > 2}"
+puts "Is it greater than equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
+
+
+# 4
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
+
+
+# 5
+
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie in 2009
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Lets talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height}."
+
+
+# 6
+
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+
+
+# 7
+
+puts "Mary had a little lamb."
+puts "Its fleece was white and #{'snow'}."
+puts "And everywhere that Mary went."
+puts "." * 10 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+
+# 8
+
+formatter = "%{first} %{second} %{third} %{fourth}"
+
+puts formatter % {first: 1, second:2, third:3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right.",
+  third: "But it did't sind.",
+  fourth: "So I said goodnight."
+}
+
+
+# 9
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+
+# 10
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+
+# 11
+
+print "How old are you?"
+age = gets.chomp
+print "How tall are you?"
+height = gets.chomp
+print "How much do you weigh?"
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+# 12
+
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+
+#13
+
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+
+# 14
+
+user_name = ARGV.first
+prompt = '>'
+
+puts "Hi #{user_name}"
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user_name}?"
+puts prompt
+likes = $stdin.gets.chomp
+
+puts "Where do you live #{user_name}?"
+puts prompt
+lives = $stdin.gets.chomp
+
+puts "What kind of computer do you have?", prompt
+computer = $stdin.gets.chomp
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}.  Not sure where that is.
+And you have a #{computer} computer.  Nice.
+"""
+
+
+# 15
+
+filename = ARGV.first
+
+txt = open(filename)
+
+
+puts "Here's your file #{filename}:"
+print txt.read
+
+
+print "Type the filename again: "
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
+
+
+# 16
+
+filename = ARGV.first
+
+puts "We're going to erase #{filename}"
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do want that, hit RETURN."
+
+$stdin.gets
+
+puts "Opening the file..."
+target = open(filename, 'w')
+
+puts "Truncating the file. Goodbaye!"
+target.truncate (0)
+
+puts "Now I'm going to ask you for three lines."
+
+print "line 1: "
+line1 = $stdin.gets.chomp
+print "line 2: "
+line2 = $stdin.gets.chomp
+print "line 3: "
+line3 = $stdin.gets.chomp
+
+puts "I'm goingt to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+puts "And finally, we close it."
+target.close
+
+
+# 17
+
+from_file, to_file = ARGV
+
+puts "Copying from #{from_file} to #{to_file}"
+
+in_file = open(from_file)
+indata = in_file.read
+
+puts "The input file is #{indata.length} bytes long"
+
+puts "Does the output file exist? #{File.exist?(to_file)}"
+puts "Ready, hit RETURN to continue, CTRL-C to abort."
+$stdin.gets
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+puts "Alright, all done."
+
+out_file.close
+in_file.close
