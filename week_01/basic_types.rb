f7b5fcb55115677ca7ef3eacf32b62b2df9c54cb
ViktoriaK <viktoria.kluckner@gmail.com>
2016-10-24 19:10:41 +0200

    Extract exercise 15 from basic_types

--- week_01/basic_types.rb
+++ week_01/basic_types.rb
@@ -262,23 +262,6 @@ And you have a #{computer} computer.  Nice.
 """
 
 
-# 15
-
-filename = ARGV.first
-
-txt = open(filename)
-
-
-puts "Here's your file #{filename}:"
-print txt.read
-
-
-print "Type the filename again: "
-file_again = $stdin.gets.chomp
-
-txt_again = open(file_again)
-
-print txt_again.read
 
 
 # 16
